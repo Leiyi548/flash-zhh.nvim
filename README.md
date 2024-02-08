ecb949ff0d547c988f124e60ddac6bb48ea7179e
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-02-09 00:18:16 +0800

    只搜一个字符的 flash 跳转实现！

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# flash-zh.nvim
+# flash-zhh.nvim
 
-基于[flash.nvim](https://github.com/folke/flash.nvim)以及小鹤双拼，neovim 中文跳转插件。
+## 为什么叫 zhh.nvim
+
+zhh 在虎码中打出来是虎！所以叫 zhh.nvim
+
+基于[flash.nvim](https://github.com/folke/flash.nvim) 和 [虎码](https://tiger-code.com/)，neovim 中文跳转插件。
 
 ![iShot_2023-10-05_19 32 53](https://github.com/rainzm/flash-zh.nvim/assets/22927169/4c3ca124-0fee-48a2-b7c6-17391afe8d0e)
 
@@ -12,12 +16,12 @@
 ```lua
 return {
 	{
-		"rainzm/flash-zh.nvim",
+		"Leiyi548/flash-zhh.nvim",
 		event = "VeryLazy",
 		dependencies = "folke/flash.nvim",
 		keys = {
 			{
-				"s",
+				"S",
 				mode = { "n", "x", "o" },
 				function()
 					require("flash-zh").jump()
@@ -45,9 +49,14 @@ label 使用大写字母，这样可以避免和拼音冲突。
 
 **如果想要跳转的地方没有 label 出现，接着输入即可，和查找一样。**
 
+## 缺点
+
+- 只能搜索一个字，不能连续搜索。（后续尝试优化）
+
 ## 感谢
 
 - [hop-zh-by-flypy](https://github.com/zzhirong/hop-zh-by-flypy)
+- [flash-zh.nvim](https://github.com/rain-zm/flash-zh)
 
 ## 推荐
 
--- lua/flash-zhh/init.lua
+++ lua/flash-zhh/init.lua
@@ -1,5 +1,6 @@
 local flash = require("flash")
 local zhh = require("flash-zhh.zhh")
+local state = require("flash").state
 
 local M = {}
 
@@ -9,39 +10,52 @@ function M.jump(opts)
 		search = {
 			mode = M._zh_mode,
 		},
+		-- 如果有 continue，那么上次的搜索还在，就继续上次搜索。
+		continue  = false
 	}, opts or {})
 	flash.jump(opts)
 end
 
 function M._zh_mode(str)
 	local regexs = {}
-	while string.len(str) > 3 then
-		-- 当我们输入4个字符的时候，agti → 装
-		-- 当我们输入5个字符的时候，比如 agtit，那么就会先把 regexs[4] = agti|装 弄上
-		-- 后面就会截取只剩 t，再进行 1 个字符区配。
-		regexs[#regexs + 1] = zhh.char4patterns[string.sub(str,1,4)]
-		-- 4 个字符不足 5，获得空字符
-		str = string.sub(str,5)
-	end
+	-- while string.len(str) > 3 then
+	-- 	-- 当我们输入4个字符的时候，agti → 装
+	-- 	-- 当我们输入5个字符的时候，比如 agtit，那么就会先把 regexs[4] = agti|装 弄上
+	-- 	-- 后面就会截取只剩 t，再进行 1 个字符区配。
+	-- 	regexs[#regexs + 1] = zhh.char4patterns[string.sub(str,1,4)]
+	-- 	-- 4 个字符不足 5，获得空字符
+	-- 	str = string.sub(str,5)
+	-- end
 	-- 如果只有一个字符，那么就直接拿这个字符去区配
+	while string.len(str) > 1 do
+		-- 如果这个时候，我们输入两个字符，那么也是直接区配
+		if string.len(str) == 2 then
+			-- 假如我要区配 他
+			-- 那么我就是输入 je
+			-- regexs[2] = [[\%(je\|[他]\)]]
+			-- 就是区配 je 或 他
+			regexs[#regexs + 1] = zhh.char2patterns[string.sub(str,1,2)]
+			print(vim.inspect(regexs))
+		end
+		if string.len(str) == 3 then
+			regexs[#regexs + 1] = zhh.char3patterns[string.sub(str,1,3)]
+			print(vim.inspect(regexs))
+		end
+		if string.len(str) == 4 then
+			regexs[#regexs + 1] = zhh.char4patterns[string.sub(str,1,4)]
+			print(vim.inspect(regexs))
+		end
+		str = string.sub(str,5)
+		-- 如果超过五个就退出 flash-zhh.nvim
+	end
 	if string.len(str) == 1 then
 		-- #regexs 代表的是长度
 		-- 这个时候 #regexs 代表是0，那么 #regexs + 1 = 1
 		regexs[#regexs + 1] = zhh.char1patterns[str]
+		print(vim.inspect(regexs))
 		-- 假如区配 “我”，注意 lua 数组是从 1 开始
 		-- regexs[1] = [[\%([t我]\)]]
 		-- 就是区配 t 或 我
-	end
-	-- 如果这个时候，我们输入两个字符，那么也是直接区配
-	if string.len(str) == 2 then
-		-- 假如我要区配 他
-		-- 那么我就是输入 je
-		-- regexs[2] = [[\%(je\|[他]\)]]
-		-- 就是区配 je 或 他
-		regexs[#regexs + 1] = zhh.char2patterns[string.sub(str,1,2)]
-	end
-	if string.len(str) == 3 then
-		regexs[#regexs + 1] = zhh.char3patterns[string.sub(str,1,3)]
 	end
 	local ret = table.concat(regexs)
 	return ret, ret
