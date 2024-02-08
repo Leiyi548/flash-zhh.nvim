81d94a931d86c34d9aa59ed22dd842b3779ffd69
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-02-08 14:46:00 +0800

    更新下区配逻辑，看看有没有用

--- lua/flash-zhh/init.lua
+++ lua/flash-zhh/init.lua
@@ -15,17 +15,33 @@ end
 
 function M._zh_mode(str)
 	local regexs = {}
-	if string.len(str) == 1 then
-		regexs[#regexs + 1] = zhh.char1patterns[str]
+	while string.len(str) > 3 then
+		-- 当我们输入4个字符的时候，agti → 装
+		-- 当我们输入5个字符的时候，比如 agtit，那么就会先把 regexs[4] = agti|装 弄上
+		-- 后面就会截取只剩 t，再进行 1 个字符区配。
+		regexs[#regexs + 1] = zhh.char4patterns[string.sub(str,1,4)]
+		-- 4 个字符不足 5，获得空字符
+		str = string.sub(str,5)
 	end
+	-- 如果只有一个字符，那么就直接拿这个字符去区配
+	if string.len(str) == 1 then
+		-- #regexs 代表的是长度
+		-- 这个时候 #regexs 代表是0，那么 #regexs + 1 = 1
+		regexs[#regexs + 1] = zhh.char1patterns[str]
+		-- 假如区配 “我”，注意 lua 数组是从 1 开始
+		-- regexs[1] = [[\%([t我]\)]]
+		-- 就是区配 t 或 我
+	end
+	-- 如果这个时候，我们输入两个字符，那么也是直接区配
 	if string.len(str) == 2 then
-		regexs[#regexs + 1] = zhh.char2patterns[str]
+		-- 假如我要区配 他
+		-- 那么我就是输入 je
+		-- regexs[2] = [[\%(je\|[他]\)]]
+		-- 就是区配 je 或 他
+		regexs[#regexs + 1] = zhh.char2patterns[string.sub(str,1,2)]
 	end
 	if string.len(str) == 3 then
-		regexs[#regexs + 1] = zhh.char3patterns[str]
-	end
-	if string.len(str) == 4 then
-		regexs[#regexs + 1] = zhh.char4patterns[str]
+		regexs[#regexs + 1] = zhh.char3patterns[string.sub(str,1,3)]
 	end
 	local ret = table.concat(regexs)
 	return ret, ret
